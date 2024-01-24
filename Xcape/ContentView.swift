d4a3ba4e421b1dd36faa955c460869873d4ad86d
Punclock AI <support@punchclock.ai>
2024-01-24 12:52:16 -0400

    Initial Commit

---
+++ Xcape/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Xcape
+//
+//  Created by Wilson Jno-Baptiste on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcape/XcapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcapeApp.swift
+//  Xcape
+//
+//  Created by Wilson Jno-Baptiste on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct XcapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
